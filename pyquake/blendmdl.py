20bedaced2c5feecdf0cdd5b207b650c518ed49d
Matthew Earl <git@matthewearl.com>
2019-04-29 22:30:42 +0100

    blendmdl:  Make a separate object for each disjoint triangle set

--- pyquake/blendmdl.py
+++ pyquake/blendmdl.py
@@ -10,10 +10,10 @@ import numpy as np
 from . import pak, mdl, blendmat
 
 
-def _create_block(obj, simple_frame):
+def _create_block(obj, simple_frame, vert_map):
     block = obj.shape_key_add(name=simple_frame.name)
-    for vert, co in zip(block.data, simple_frame.frame_verts):
-        vert.co = co
+    for old_vert_idx, block_vert in zip(vert_map, block.data):
+        block_vert.co = simple_frame.frame_verts[old_vert_idx]
     return block
 
 
@@ -23,38 +23,39 @@ class BlendMdl:
     blocks: Dict
     obj: bpy_types.Object
 
-    def animate(self, frames, fps=30):
-        prev_block = None
-        prev_time = None
-        for time, frame_num in frames:
-            simple_frame = self.am.frames[frame_num].frame
 
-            block = self.blocks[simple_frame.name]
+def _animate(am, blocks, obj, frames, fps=30):
+    prev_block = None
+    prev_time = None
+    for time, frame_num in frames:
+        simple_frame = am.frames[frame_num].frame
 
-            block.value = 1.0
-            block.keyframe_insert('value', frame=int(fps * time))
-            if prev_block:
-                block.value = 0.0
-                block.keyframe_insert('value', frame=int(fps * prev_time))
-                prev_block.value = 0.0
-                prev_block.keyframe_insert('value', frame=int(fps * time))
+        block = blocks[simple_frame.name]
 
-            prev_block = block
-            prev_time = time
+        block.value = 1.0
+        block.keyframe_insert('value', frame=int(fps * time))
+        if prev_block:
+            block.value = 0.0
+            block.keyframe_insert('value', frame=int(fps * prev_time))
+            prev_block.value = 0.0
+            prev_block.keyframe_insert('value', frame=int(fps * time))
 
-        for c in self.obj.data.animation_data.action.fcurves:
-            for kfp in c.keyframe_points:
-                kfp.interpolation = 'LINEAR'
+        prev_block = block
+        prev_time = time
+
+    for c in obj.data.animation_data.action.fcurves:
+        for kfp in c.keyframe_points:
+            kfp.interpolation = 'LINEAR'
 
 
-def _set_uvs(mesh, am):
+def _set_uvs(mesh, am, tri_set):
     mesh.uv_layers.new()
 
     bm = bmesh.new()
     bm.from_mesh(mesh)
     uv_layer = bm.loops.layers.uv[0]
 
-    for tri_idx, bm_face in enumerate(bm.faces):
+    for bm_face, tri_idx in zip(bm.faces, tri_set):
         tcs = am.get_tri_tcs(tri_idx)
 
         for bm_loop, (s, t) in zip(bm_face.loops, tcs):
@@ -63,47 +64,71 @@ def _set_uvs(mesh, am):
     bm.to_mesh(mesh)
 
 
-def load_mdl(pak_root, mdl_name, obj_name, skin=0, fps=30):
+def _simplify_pydata(verts, tris):
+    vert_map = [] 
+    new_tris = []
+    for tri in tris:
+        new_tri = []
+        for vert_idx in tri:
+            if vert_idx not in vert_map:
+                vert_map.append(vert_idx)
+            new_tri.append(vert_map.index(vert_idx))
+        new_tris.append(new_tri)
+
+    return ([verts[old_vert_idx] for old_vert_idx in vert_map], [], new_tris), vert_map
+
+
+def load_mdl(pak_root, mdl_name, obj_name, frames, skin=0, fps=30):
+    frames = list(frames)
+
     # Load the alias model
     fs = pak.Filesystem(pak_root)
     fname = f'progs/{mdl_name}.mdl'
     am = mdl.AliasModel(io.BytesIO(fs[fname]))
 
-    # Create the mesh and object
-    mesh = bpy.data.meshes.new(obj_name)
-    mesh.from_pydata([list(v) for v in am.frames[0].frame.frame_verts], [], [list(t) for t in am.tris])
-    obj = bpy.data.objects.new(obj_name, mesh)
+    obj = bpy.data.objects.new(obj_name, None)
     bpy.context.scene.collection.objects.link(obj)
+    for tri_set_idx, tri_set in enumerate(am.disjoint_tri_sets):
+        # Create the mesh and object
+        subobj_name = f"{obj_name}_triset{tri_set_idx}"
+        mesh = bpy.data.meshes.new(subobj_name)
+        pydata, vert_map = _simplify_pydata([list(v) for v in am.frames[0].frame.frame_verts],
+                                            [list(am.tris[t]) for t in tri_set])
+        mesh.from_pydata(*pydata)
+        subobj = bpy.data.objects.new(subobj_name, mesh)
+        subobj.parent = obj
+        bpy.context.scene.collection.objects.link(subobj)
 
-    # Create shape key blocks, used for animation.
-    blocks = {}
-    for frame in am.frames:
-        if frame.frame_type != mdl.FrameType.SINGLE:
-            raise Exception(f"Frame type {frame.frame_type} not supported")
-        simple_frame = frame.frame
-        if simple_frame.name in blocks:
-            raise Exception("Duplicate frame name")
-        blocks[simple_frame.name] = _create_block(obj, simple_frame)
+        # Create shape key blocks, used for animation.
+        blocks = {}
+        for frame in am.frames:
+            if frame.frame_type != mdl.FrameType.SINGLE:
+                raise Exception(f"Frame type {frame.frame_type} not supported")
+            simple_frame = frame.frame
+            if simple_frame.name in blocks:
+                raise Exception("Duplicate frame name")
+            blocks[simple_frame.name] = _create_block(subobj, simple_frame, vert_map)
+        _animate(am, blocks, subobj, frames, fps)
 
-    # Set up material
-    mat_name = f"{mdl_name}_{skin}"
-    if mat_name not in bpy.data.materials:
-        pal = np.fromstring(fs['gfx/palette.lmp'], dtype=np.uint8).reshape(256, 3) / 255
-        pal = np.concatenate([pal, np.ones(256)[:, None]], axis=1)
+        # Set up material
+        mat_name = f"{mdl_name}_{skin}"
+        if mat_name not in bpy.data.materials:
+            pal = np.fromstring(fs['gfx/palette.lmp'], dtype=np.uint8).reshape(256, 3) / 255
+            pal = np.concatenate([pal, np.ones(256)[:, None]], axis=1)
 
-        mat, nodes, links = blendmat.new_mat(mat_name)
-        array_im, fullbright_array_im = blendmat.array_ims_from_indices(mat_name, pal, am.skins[skin])
-        im = blendmat.im_from_array(mat_name, array_im)
-        if fullbright_array_im is not None:
-            fullbright_im = blendmat.im_from_array(f"{mat_name}_fullbright", fullbright_array_im)
-            blendmat.setup_fullbright_material(nodes, links, im, fullbright_im, 1.0)
-        else:
-            blendmat.setup_diffuse_material(nodes, links, im)
-    mat = bpy.data.materials[mat_name]
+            mat, nodes, links = blendmat.new_mat(mat_name)
+            array_im, fullbright_array_im = blendmat.array_ims_from_indices(mat_name, pal, am.skins[skin])
+            im = blendmat.im_from_array(mat_name, array_im)
+            if fullbright_array_im is not None:
+                fullbright_im = blendmat.im_from_array(f"{mat_name}_fullbright", fullbright_array_im)
+                blendmat.setup_fullbright_material(nodes, links, im, fullbright_im, 1.0)
+            else:
+                blendmat.setup_diffuse_material(nodes, links, im)
+        mat = bpy.data.materials[mat_name]
 
-    # Apply the material
-    mesh.materials.append(mat)
-    _set_uvs(mesh, am)
+        # Apply the material
+        mesh.materials.append(mat)
+        _set_uvs(mesh, am, tri_set)
 
     return BlendMdl(am, blocks, obj)
 
