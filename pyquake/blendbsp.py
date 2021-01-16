fullbright obj: bbox off by one, and fix truncs through verts
Taking the bbox upper bound as the maximum index actually drops the
final row/column, eg. a 2x2 texture completely fullbright would have had
bbox [(0, 0), (1, 1)] so when truncated would have been 1x1 in size.

The other fix handles the case where _truncate_verts runs through an
existing vertex. Before this produced duplicate verts, which apparently
blender handles by just omitting the face. The fix is to change what
is considered to be infront of the cutting plane from >= to >.  I
imagine there could still be floating point bugs here that mean the
function could still produce duplicate verts, but hopefully Quake
mostly working with round numbers means we won't see such an issue.

pyquake/blendbsp.py
@@ -185,7 +185,7 @@ def _get_bbox(a):
     out = []
     for axis in range(2):
         b = np.where(np.any(a, axis=axis))
-        out.append([np.min(b), np.max(b)])
+        out.append([np.min(b), np.max(b) + 1])
     return np.array(out).T
 
 
@@ -228,12 +228,12 @@ def _truncate_face(vertices, normal, plane_dist):
         dist = np.dot(vert, normal) - plane_dist
         prev_dist = np.dot(prev_vert, normal) - plane_dist
 
-        if (prev_dist >= 0) != (dist >= 0):
+        if (prev_dist > 0) != (dist > 0):
             alpha = -dist / (prev_dist - dist)
             new_vert = tuple(alpha * np.array(prev_vert) + (1 - alpha) * np.array(vert))
             new_vertices.append(new_vert)
 
-        if dist >= 0:
+        if dist > 0:
             new_vertices.append(vert)
 
         prev_vert = vert