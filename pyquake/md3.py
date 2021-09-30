be342386a97d251573f824157732e50b2f3409d9
Matthew Earl <git@matthewearl.com>
2021-09-30 09:40:56 +0100

    initial md3 parsing

---
+++ pyquake/md3.py
@@ -0,0 +1,102 @@
+# Copyright (c) 2021 Matthew Earl
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+#     The above copyright notice and this permission notice shall be included
+#     in all copies or substantial portions of the Software.
+# 
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+#     NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#     OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+#     USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+__all__ = (
+    'MalformedMD3Error',
+    'MD3',
+    'MD3Surface',
+)
+
+import struct
+
+import numpy as np
+
+
+class MalformedMD3Error(Exception):
+    pass
+
+
+def _read(f, n):
+    b = f.read(n)
+    if len(b) < n:
+        raise MalformedMD3Error("File ended unexpectedly")
+    return b
+
+
+def _read_struct(f, struct_fmt, post_func=None):
+    size = struct.calcsize(struct_fmt)
+    out = struct.unpack(struct_fmt, _read(f, size))
+    if post_func is not None:
+        out = post_func(*out)
+    return out
+
+
+class MD3Surface:
+    def _read_tris(self, f, offset, num_triangles):
+        f.seek(offset)
+        return np.frombuffer(_read(f, 4 * 3 * num_triangles), dtype=np.int32).reshape((num_triangles, 3))
+
+    def _read_verts(self, f, offset, num_verts):
+        f.seek(offset)
+        vert_dtype = np.dtype([('verts', np.int16, 3), ('lng', np.uint8), ('lat', np.uint8)])
+        vert_data = (
+            np.frombuffer(_read(f, vert_dtype.itemsize * num_verts * self.num_frames), dtype=vert_dtype)
+                .reshape((self.num_frames, num_verts))
+        )
+        verts = vert_data['verts'] / 64.
+        lat = vert_data['lat'] * np.pi / 128
+        lng = vert_data['lng'] * np.pi / 128
+        normals = np.stack([
+            np.cos(lat) * np.sin(lng),
+            np.sin(lat) * np.sin(lng),
+            np.cos(lng)
+        ], axis=-1)
+
+        return verts, normals
+
+    def __init__(self, f, offset):
+        f.seek(offset)
+        (self.ident, name, flags, self.num_frames, num_shaders, num_verts, num_triangles, triangles_offset,
+         shaders_offset, tc_offset, vert_offset, self.next_offset) = _read_struct(f, '<4s64sllllllllll')
+        self.name = name[:name.index(b'\0')].decode('ascii')
+
+        self.tris = self._read_tris(f, offset + triangles_offset, num_triangles)
+        self.verts, self.normals = self._read_verts(f, offset + vert_offset, num_verts)
+
+
+class MD3:
+    def __init__(self, f):
+        (ident, version, name, flags, num_frames, num_tags, num_surfaces, num_skins, frames_offset, tags_offset,
+         surfaces_offset, eof_offset) = _read_struct(f, "<4sl64slllllllll")
+        self.name = name[:name.index(b'\0')].decode('ascii')
+
+        if ident != b'IDP3': 
+            raise MalformedMD3Error(f'Bad ident: {ident}')
+
+        if version != 15:
+            raise MalformedMD3Error(f'Only version 15 is supported, not {version}')
+
+        self.surfaces = []
+        for surface_idx in range(num_surfaces):
+            surface = MD3Surface(f, surfaces_offset)
+            surfaces_offset += surface.next_offset
+            self.surfaces.append(surface)
+
