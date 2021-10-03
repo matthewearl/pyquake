bd6e322a70e6ae07d316aef7959933b1a1c8c72d
Matthew Earl <git@matthewearl.com>
2021-10-03 10:25:13 +0100

    add tags into blendmd3 as empties

--- pyquake/blendmd3.py
+++ pyquake/blendmd3.py
@@ -4,9 +4,11 @@ __all__ = (
 
 
 import dataclasses
-import bpy
-import numpy as np
 from typing import List, Tuple
+
+import bpy
+import mathutils
+import numpy as np
 
 from . import md3
 
@@ -18,14 +20,34 @@ def _create_shape_key(surf_obj, surf, frame_num):
     return shape_key
 
 
+def _quat_for_tag(tag):
+    q = mathutils.Quaternion([1, 0, 0], 0)
+    q.rotate(mathutils.Matrix(tag['axis']))
+    return q
+
+
 def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
               times: np.ndarray, anim_idxs: np.ndarray,
               obj_name: str, fps: float):
     obj = bpy.data.objects.new(obj_name, None)
-    surf_objs = []
     bpy.context.scene.collection.objects.link(obj)
 
+    # Create an object for each tag.
+    tag_objs = {}
+    for tag_name, tags in m.tags.items():
+        tag_obj = bpy.data.objects.new(f'{obj_name}_{tag_name}', None)
+        bpy.context.scene.collection.objects.link(tag_obj)
+        tag_obj.rotation_quaternion = _quat_for_tag(tags[0])
+        tag_obj.location = tags[0]['origin']
+        tag_obj.rotation_mode = 'QUATERNION'
+        tag_obj.parent = obj
+        tag_objs[tag_name] = tag_obj
+
+    # Create shape keys.
+    # TODO: Currently shape_keys is a flat list, but should be 2D with surf idx and frame as indices, and shapes should
+    # be set for each surface.  This is most likely broken for animated multi-surface models.
     shape_keys = []
+    surf_objs = []
     for surf in m.surfaces:
         surf_obj_name = f'{obj_name}_{surf.name}'
         mesh = bpy.data.meshes.new(surf_obj_name)
@@ -49,14 +71,17 @@ def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
     end_times = times[change_idxs[1:]]
     changed_anim_idxs = anim_idxs[change_idxs[:-1]]
 
+    # Each loop is a period where the animation index remains the same
     prev_anim_frame = None
     prev_blender_frame = None
     for anim_idx, start_time, end_time in zip(changed_anim_idxs, start_times, end_times):
-        anim_idx &= ~128    # This bit is flipped to force an anim restart, but it should otherwise be ignored.
+        # This bit is flipped to force an anim restart, but it should otherwise be ignored.
+        anim_idx &= ~128
 
         start_frame = int(start_time * fps)
         end_frame = int(end_time * fps)
 
+        # Work out which frames should be played during this period.
         anim = anim_info.anims[anim_idx]
         num_anim_frames = int(anim.fps * (end_time - start_time))
         anim_frames = np.arange(num_anim_frames)
@@ -67,14 +92,12 @@ def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
             )
         else:
             anim_frames = np.minimum(anim_frames, anim.num_frames - 1)
-
         anim_frames += anim.first_frame
         anim_frame_times = start_time + np.arange(num_anim_frames) / anim.fps
 
+        # Insert keyframes according to the animations.
         for anim_frame, anim_frame_time in zip(anim_frames, anim_frame_times):
             blender_frame = int(fps * anim_frame_time)
-
-            # if prev frame is same as previous frame then this no longer works.
             if prev_blender_frame is not None and prev_anim_frame != anim_frame:
                 shape_keys[anim_frame].value = 0
                 shape_keys[anim_frame].keyframe_insert('value', frame=prev_blender_frame)
@@ -88,6 +111,15 @@ def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
             prev_anim_frame = anim_frame
             prev_blender_frame = blender_frame
 
+            for tag_name, tag_obj in tag_objs.items():
+                tag = m.tags[tag_name][anim_frame]
+
+                tag_obj.rotation_quaternion = _quat_for_tag(tag)
+                tag_obj.keyframe_insert('rotation_quaternion', frame=blender_frame)
+                tag_obj.location = tag['origin']
+                tag_obj.keyframe_insert('location', frame=blender_frame)
+
+    # Make shape keys linearly interpolated.
     for surf_obj in surf_objs:
         for c in surf_obj.data.shape_keys.animation_data.action.fcurves:
             for kfp in c.keyframe_points:
@@ -105,6 +137,10 @@ def add_player(lower_md3: md3.MD3, upper_md3: md3.MD3, head_md3: md3.MD3,
     bpy.context.scene.collection.objects.link(root_obj)
 
     lower_obj = add_model(lower_md3, anim_info, pmove_frames.times, pmove_frames.leg_anim_idxs, 'lower', fps)
+
+    upper_obj = bpy.data.objects.new(obj_name, None)
+    bpy.context.scene.collection.objects.link(upper_obj)
+    upper_obj.parent = lower_obj
 
     for time, origin, angles in zip(pmove_frames.times,
                                     pmove_frames.origins,
