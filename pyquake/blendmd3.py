1aa3d6e21051fbd73dd81e9aee8c900f04c31834
Matthew Earl <git@matthewearl.com>
2021-10-03 14:59:08 +0100

    add head onto player

--- pyquake/blendmd3.py
+++ pyquake/blendmd3.py
@@ -33,56 +33,17 @@ def _quat_from_axis(axis, prev_quat):
     return q
 
 
-def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
-              times: np.ndarray, anim_idxs: np.ndarray,
-              obj_name: str, origin_tag_name: Optional[str], fps: float):
-    obj = bpy.data.objects.new(obj_name, None)
-    bpy.context.scene.collection.objects.link(obj)
+def _animate_model(m: md3.MD3, origin_obj, surf_objs, tag_objs, anim_info: md3.AnimationInfo,
+                   times: np.ndarray, anim_idxs: np.ndarray,
+                   origin_tag_name: Optional[str], fps: float):
+    if len({surf.num_frames for surf in m.surfaces}) != 1:
+        raise MalformedMD3Error('Surfaces have different numbers of frames')
 
-    if origin_tag_name is not None:
-        # Set up an origin object, which applies the inverse transformation of a named tag.
-        origin_obj = bpy.data.objects.new(f'{obj_name}_origin', None)
-        bpy.context.scene.collection.objects.link(origin_obj)
-        origin_obj.parent = obj
-
-        origin_tag = m.tags[origin_tag_name][0]
-        origin_obj.rotation_quaternion = _quat_from_axis(origin_tag['axis'].T, None)
-        origin_obj.location = -origin_tag['axis'].T @ origin_tag['origin']
-        origin_obj.rotation_mode = 'QUATERNION'
-    else:
-        origin_obj = obj
-
-    # Create an object for each tag.
-    tag_objs = {}
-    for tag_name, tags in m.tags.items():
-        tag_obj = bpy.data.objects.new(f'{obj_name}_{tag_name}', None)
-        bpy.context.scene.collection.objects.link(tag_obj)
-        tag_obj.rotation_quaternion = _quat_from_axis(tags[0]['axis'], None)
-        tag_obj.location = tags[0]['origin']
-        tag_obj.rotation_mode = 'QUATERNION'
-        tag_obj.parent = origin_obj
-        tag_objs[tag_name] = tag_obj
-
-    # Create shape keys.
-    # TODO: Currently shape_keys is a flat list, but should be 2D with surf idx and frame as indices, and shapes should
-    # be set for each surface.  This is most likely broken for animated multi-surface models.
+    # Create shape keys for each surf/frame pair.
     shape_keys = []
-    surf_objs = []
-    for surf in m.surfaces:
-        surf_obj_name = f'{obj_name}_{surf.name}'
-        mesh = bpy.data.meshes.new(surf_obj_name)
-        mesh.from_pydata([list(v) for v in surf.verts[0]],
-                         [],
-                         [list(t) for t in surf.tris])
-
-        surf_obj = bpy.data.objects.new(surf_obj_name, mesh)
-        surf_objs.append(surf_obj)
-        bpy.context.scene.collection.objects.link(surf_obj)
-
-        surf_obj.parent = origin_obj
-
-        for frame_num in range(surf.num_frames):
-            shape_keys.append(_create_shape_key(surf_obj, surf, frame_num))
+    for frame_num in range(m.surfaces[0].num_frames):
+        shape_keys.append([_create_shape_key(surf_obj, surf, frame_num)
+                           for surf, surf_obj in zip(m.surfaces, surf_objs)])
 
     # De-duplicate frames
     change_idxs = np.where(np.diff(anim_idxs) != 0)[0]
@@ -115,18 +76,22 @@ def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
         anim_frames += anim.first_frame
         anim_frame_times = start_time + np.arange(num_anim_frames) / anim.fps
 
+        def set_shape_key(anim_frame, blender_frame, value):
+            # Set a keyframe for shape-key value for each surface.
+            for sk in shape_keys[anim_frame]:
+                sk.value = value
+                sk.keyframe_insert('value', frame=blender_frame)
+
         # Insert keyframes according to the animations.
         for anim_frame, anim_frame_time in zip(anim_frames, anim_frame_times):
             blender_frame = int(fps * anim_frame_time)
 
             # Animate shape keys
             if prev_blender_frame is not None and prev_anim_frame != anim_frame:
-                shape_keys[anim_frame].value = 0
-                shape_keys[anim_frame].keyframe_insert('value', frame=prev_blender_frame)
-                shape_keys[prev_anim_frame].value = 0
-                shape_keys[prev_anim_frame].keyframe_insert('value', frame=blender_frame)
-            shape_keys[anim_frame].value = 1
-            shape_keys[anim_frame].keyframe_insert('value', frame=blender_frame)
+                set_shape_key(anim_frame, prev_blender_frame, 0)
+                set_shape_key(prev_anim_frame, blender_frame, 0)
+            set_shape_key(anim_frame, blender_frame, 1)
+
             prev_anim_frame = anim_frame
             prev_blender_frame = blender_frame
 
@@ -153,6 +118,54 @@ def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
             for kfp in c.keyframe_points:
                 kfp.interpolation = 'LINEAR'
 
+
+def add_model(m: md3.MD3, anim_info: md3.AnimationInfo,
+              times: np.ndarray, anim_idxs: np.ndarray,
+              obj_name: str, origin_tag_name: Optional[str], fps: float):
+    obj = bpy.data.objects.new(obj_name, None)
+    bpy.context.scene.collection.objects.link(obj)
+
+    if origin_tag_name is not None:
+        # Set up an origin object, which applies the inverse transformation of a named tag.
+        origin_obj = bpy.data.objects.new(f'{obj_name}_origin', None)
+        bpy.context.scene.collection.objects.link(origin_obj)
+        origin_obj.parent = obj
+
+        origin_tag = m.tags[origin_tag_name][0]
+        origin_obj.rotation_quaternion = _quat_from_axis(origin_tag['axis'].T, None)
+        origin_obj.location = -origin_tag['axis'].T @ origin_tag['origin']
+        origin_obj.rotation_mode = 'QUATERNION'
+    else:
+        origin_obj = obj
+
+    # Create surface objects.
+    surf_objs = []
+    for surf in m.surfaces:
+        surf_obj_name = f'{obj_name}_{surf.name}'
+        mesh = bpy.data.meshes.new(surf_obj_name)
+        mesh.from_pydata([list(v) for v in surf.verts[0]],
+                         [],
+                         [list(t) for t in surf.tris])
+
+        surf_obj = bpy.data.objects.new(surf_obj_name, mesh)
+        surf_objs.append(surf_obj)
+        bpy.context.scene.collection.objects.link(surf_obj)
+        surf_obj.parent = origin_obj
+
+    # Create an object for each tag.
+    tag_objs = {}
+    for tag_name, tags in m.tags.items():
+        tag_obj = bpy.data.objects.new(f'{obj_name}_{tag_name}', None)
+        bpy.context.scene.collection.objects.link(tag_obj)
+        tag_obj.rotation_quaternion = _quat_from_axis(tags[0]['axis'], None)
+        tag_obj.location = tags[0]['origin']
+        tag_obj.rotation_mode = 'QUATERNION'
+        tag_obj.parent = origin_obj
+        tag_objs[tag_name] = tag_obj
+
+    if anim_info is not None:
+        _animate_model(m, origin_obj, surf_objs, tag_objs, anim_info, times, anim_idxs, origin_tag_name, fps)
+
     return obj, tag_objs
 
 
@@ -171,6 +184,10 @@ def add_player(lower_md3: md3.MD3, upper_md3: md3.MD3, head_md3: md3.MD3,
                                           pmove_frames.times, pmove_frames.torso_anim_idxs,
                                           'upper', 'tag_torso', fps)
     upper_obj.parent = lower_tag_objs['tag_torso']
+
+    head_obj, head_tag_objs = add_model(head_md3, None, None, None,
+                                        'head', 'tag_head', fps)
+    head_obj.parent = upper_tag_objs['tag_head']
 
     for time, origin, angles in zip(pmove_frames.times,
                                     pmove_frames.origins,
